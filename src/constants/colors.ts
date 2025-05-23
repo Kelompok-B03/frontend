d686ee54649e4ab1ff29f0f43d51081e66b3620a
widaputri <widapk2709@gmail.com>
2025-05-23 19:56:30 +0700

    Add icon components and color constants for UI elements

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export const appColors = {
+  babyPinkLight: '#FDECF0',
+  babyTurquoiseLight: '#E0FCFA',
+  lightGrayBg: '#F3F4F6',
+  white: '#FFFFFF',
+
+  textDark: '#374151',
+  textDarkMuted: '#525E6C',
+
+  babyPinkAccent: '#D94A7B',
+  babyTurquoiseAccent: '#36A5A0',
+};

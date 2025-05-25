a1e357c6a8014070c68ba8aeadb651ebe374a758
widaputri <widapk2709@gmail.com>
2025-05-25 17:26:26 +0700

    Update axios instance base URL to use environment variable for improved configuration

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_AUTH_SERVICE_LINK,
 });
 
 // Interceptor untuk menambahkan token ke header
